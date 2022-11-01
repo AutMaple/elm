63aa4f8eae63040c876a9a6f2fd0448cc2b06811
AutMaple <autmaple609@qq.com>
2022-11-01 22:57:11 +0800

    添加 prettier 配置文件

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 80, // 单行长度80
+  useTabs: false, // 使用空格代替 tab
+  semi: true, // 句末使用分号
+  singleQuote: false, // 使用双引号
+  trailingComma: "all", // 多行时尽可能打印尾随的逗号
+  bracketSpacing: true, //在对象前后添加空格-eg: { foo: bar }
+  arrowParens: "always", //单参数箭头函数参数周围使用圆括号-eg: (x) => x
+  embeddedLanguageFormatting: "auto", //对引用代码进行格式化
+};
